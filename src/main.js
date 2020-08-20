eba9a8d82fa72c8e3c5c40244cf7c968e95cf15b
jjz <505817785@qq.com>
2020-08-20 19:38:39 +0800

    x2js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router/index.js'
 import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
+import x2js from 'x2js'
 import { Message } from 'element-ui'
 import Moment from 'moment'
 import store from './store'
@@ -13,6 +14,8 @@ import echarts from 'echarts'
 Vue.config.productionTip = false
 
 Vue.prototype.$echart = echarts
+// eslint-disable-next-line new-cap
+Vue.prototype.$x2js = new x2js()
 
 axios.defaults.baseURL = 'http://localhost:8081'
 Vue.prototype.$http = axios
