6f4191fada47851acc31652e5c890627aadc9bd7
jjz <505817785@qq.com>
2020-08-21 13:19:18 +0800

    new project

--- src/components/new/NewProject.vue
+++ src/components/new/NewProject.vue
@@ -20,11 +20,13 @@
       <el-form-item label="作者">
         <el-input v-model="projectInfo.author" :disabled="true"></el-input>
       </el-form-item>
+      <el-button type="primary" @click="_upload">上传文件（若不选择文件，则生成空模型）</el-button>
     </el-form>
     <span slot="footer" class="dialog-footer">
       <el-button @click="dialogVisible = false">取 消</el-button>
       <el-button type="primary" @click="createProject">下一步</el-button>
     </span>
+    <input type="file" id="upload_xml" @change="handle_file($event)" accept=".xml" style="display: none;">
   </el-dialog>
 </template>
 
@@ -41,6 +43,7 @@ export default {
       projectInfo: {
         info: '',
         type: '',
+        model: '',
         author: '',
         comId: ''
       }
@@ -58,6 +61,26 @@ export default {
     createProject () {
       this.dialogVisible = false
       this.newProject()
+    },
+
+    // 弹出上传文件框
+    _upload () {
+      document.getElementById('upload_xml').click()
+    },
+
+    // 处理上传的文件
+    handle_file ($event) {
+      var _this = this
+      var files = event.target.files
+      if (files != null) {
+        var file = files[0]
+        var reader = new FileReader()
+        reader.onload = function (e) {
+          _this.projectInfo.model = e.target.result
+          _this.$message('文件已添加')
+        }
+        reader.readAsText(file)
+      }
     },
 
     async newProject () {
